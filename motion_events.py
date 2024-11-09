dca824ea1d4be429c2d407fa5948286c998f0e1d
James De Ricco <james.dericco.dev@gmail.com>
2024-11-09 17:16:59 -0500

    parse_frame_time: change parser to a new filename format
    Support for the old format is removed.
    
    New filename convention example: frame_time-utc-2024-11-09T14-24-07_prediction-empty_confidence-percent-99.png

--- motion_events.py
+++ motion_events.py
@@ -3,31 +3,45 @@
 import argparse
 from datetime import datetime, timedelta
 import os
+import re
 
 
-# Frame Filename Format: 'frame_time-2024-10-02T15-54-54_prediction-jacky_confidencepercent-33.png'
-def parse_frame_time(frame_time: str) -> datetime:
+# Frame Filename Format: 'frame_time-utc-2024-11-09T14-24-07_prediction-empty_confidence-percent-99.png'
+# Deprecated unsupported format: 'frame_time-2024-10-02T15-54-54_prediction-jacky_confidencepercent-33.png'
+def parse_frame_time(frame_filename: str) -> datetime:
     """
     Parse the timestamp in the frame filename and return a datetime object.
     """
-    # Extract the timestamp from the filename format: 'frame_time-2024-10-02T15-54-54'
-    time_part = frame_time.split("_time-")[-1].split("_prediction-")[0]
+
+    pattern = r"time-utc-(\d{4}-\d{2}-\d{2}T\d{2}-\d{2}-\d{2})"
+    match = re.search(pattern, frame_filename)
+
+    if match is None:
+        raise SyntaxError(
+            f"Failed to parse '{frame_filename}'. It does not match the pattern '{pattern}'."
+        )
+
+    time_part = match.groups()[0]
     return datetime.strptime(time_part, "%Y-%m-%dT%H-%M-%S")
+
+
+def parse_frame_time_from_path(frame_path: str) -> datetime:
+    return parse_frame_time(os.path.basename(frame_path))
 
 
 def group_frame_files_by_motion_event(frame_files: list[str]) -> list[list[str]]:
     """
     Return a list of motion events, which are lists of frames that are three or less seconds apart.
     """
-    frame_files.sort(key=lambda f: parse_frame_time(f))  # Sort by timestamp
+    frame_files.sort(key=lambda f: parse_frame_time_from_path(f))  # Sort by timestamp
     motion_events = []
     current_event = []
 
     for frame_file in frame_files:
-        frame_time = parse_frame_time(frame_file)
+        frame_time = parse_frame_time_from_path(frame_file)
 
         if current_event:
-            last_frame_time = parse_frame_time(current_event[-1])
+            last_frame_time = parse_frame_time_from_path(current_event[-1])
             if (frame_time - last_frame_time) <= timedelta(seconds=3):
                 current_event.append(frame_file)
             else:
